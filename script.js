ba19386874bb47af24f287ebc7426123513e3289
juronon <uronon23@gmail.com>
2017-09-22 20:25:51 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// var expr = ('2*3+2');
+//
+// console.log( eval(expr) );
+
+
+
+// var exOn = 'Регулярные выражения';
+// var patterOne = /раж/;
+// console.log(exOn.search(patterOne));
+
+
+
+var expr = prompt("Введите выражение?", '2*3+2');
+
+alert( eval(expr) );
+
